594e883b7ea722929658ac93366e76001d5e0199
Claus Bornich <clausb@gmail.com>
2014-07-17 22:30:35 +0200

    added texture flip script

---
+++ Assets/Scripts/TextureFlip.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureFlip : MonoBehaviour 
+{
+	Vector3 v3Texture;
+
+	bool bLeft = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// normal object scale
+		v3Texture = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// go left
+		if (Input.GetKey ("d") && !bLeft)
+		{
+			// flip texture
+			bLeft = true;
+			transform.localScale = new Vector3(v3Texture.x, v3Texture.y, v3Texture.z);;
+		}
+
+		// go right
+		if (Input.GetKey ("a") && bLeft)
+		{
+			// set texture to normal position
+			bLeft = false;
+			transform.localScale = new Vector3(-v3Texture.x, v3Texture.y, v3Texture.z);;
+		}
+	
+	}
+}
