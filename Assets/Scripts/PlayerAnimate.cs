c9b0de2ac514d5226043b4b3d5b5c3284956743a
Claus Bornich <clausb@gmail.com>
2014-07-18 12:29:40 +0200

    adding movement animation

---
+++ Assets/Scripts/PlayerAnimate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimate : MonoBehaviour {
+
+	private AnimatedTextureExtendedUV animateTexture;
+
+	private bool bMoving = false;
+
+	// Use this for initialization
+	void Start () {
+		animateTexture = gameObject.GetComponent<AnimatedTextureExtendedUV>();
+		//control = GameObject.Find("GameControl").GetComponent<GameControlScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("a") || Input.GetKey ("d") && !bMoving)
+		{
+			bMoving = true;
+			animateTexture.rowNumber = 1;
+		}
+		else if (bMoving)
+		{
+			bMoving = false;
+			animateTexture.rowNumber = 0;
+		}
+	}
+}
