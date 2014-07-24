d3247c3cecc31b8796b63da2983969e70619ac6c
Claus Bornich <clausb@gmail.com>
2014-07-24 14:33:36 +0200

    added parallax effect to forest level

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -39,6 +39,25 @@ public class Parallax : MonoBehaviour {
 	// called each fram
 	void Update () 
 	{
-	
+		// for each background
+		for (int i = 0; i < backgrounds.Length; i++)
+		{
+			float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+			// set target x position which is current position plus parallax
+			float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+			// TODO: also for Y-axis
+			// float backgroundTargetPosY = backgrounds[i].position.y + parallax;
+
+			// create target position - background's current pos with target x pos
+			Vector3 backgroundTargetPosition = new Vector3 (backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+			// fade between current position and target position using lerp
+			backgrounds[i].position = Vector3.Lerp (backgrounds[i].position, backgroundTargetPosition, smoothing * Time.deltaTime);
+		}
+
+		// set previous camera position to camera's position at end of the frame
+		previousCamPos = cam.position;
 	}
 }
